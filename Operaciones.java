be83d805da724250f31c4e26976230cf867ead98
Ignacio Mosconi <134315575+ignamosconi@users.noreply.github.com>
2024-05-28 20:52:29 -0300

    creación de rama

--- Operaciones.java
+++ Operaciones.java
@@ -2,7 +2,6 @@ public interface Operaciones {
     //Métodos versión 0.1
     int sumar(int num1, int num2);
     
-    
     //Métodos a implementar versión 1.0
     /* 
     int restar(int num1, int num2);
