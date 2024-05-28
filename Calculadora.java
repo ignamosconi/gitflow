5cd5f51af3f3c3a4c9c2d9fa6eac16db8f57409c
Ignacio Mosconi <134315575+ignamosconi@users.noreply.github.com>
2024-05-28 20:36:55 -0300

    v 0.1 - Commit inicial

---
+++ Calculadora.java
@@ -0,0 +1,9 @@
+public class Calculadora implements Operaciones {
+
+    //Constructor (vacío porque no usamos argumentos)
+    public Calculadora() {}
+
+    public int sumar(int num1, int num2) {
+        return (num1 + num2 + num2);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String args[]) { ;
+        Calculadora calcu1 = new Calculadora();
+        System.out.println(calcu1.sumar(1,5));
+    }
+}
