4e90eb7d45f3e8c6314c8310731c26f7334eef54
Ignacio Mosconi <134315575+ignamosconi@users.noreply.github.com>
2024-05-28 21:18:54 -0300

    Añadido de multiplicación y potencia

--- Calculadora.java
+++ Calculadora.java
@@ -6,4 +6,13 @@ public class Calculadora implements Operaciones {
     public int sumar(int num1, int num2) {
         return (num1 + num2 + num2);
     }
+
+    //Métodos Igna - rama feature-multPot
+    public int multiplicar(int num1, int num2) {
+        return (num1 * num2 * num2);
+    }
+
+    public double potencia(int base, int exponente) {
+        return Math.pow(base,exponente);
+    }
 }
--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String args[]) { ;
         Calculadora calcu1 = new Calculadora();
         System.out.println(calcu1.sumar(1,5));
+
+        System.out.println(calcu1.multiplicar(2, 3));
+        System.out.println(calcu1.potencia(2,5));
     }
 }
