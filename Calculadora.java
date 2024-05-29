0baf3e371bfe9643414653ca8f39802ce6b19f2a
Bruno Lucarelli <brunolucarelli5@gmail.com>
2024-05-28 22:21:24 -0300

    Funcionalidad de resta y división

--- Calculadora.java
+++ Calculadora.java
@@ -6,4 +6,13 @@ public class Calculadora implements Operaciones {
     public int sumar(int num1, int num2) {
         return (num1 + num2);
     }
+
+    public int restar(int num1, int num2){
+        return (num1 - num2);
+    }
+
+    public double dividir(int num1, int num2){
+        return (num1 / num2);
+    }
+
 }
--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String args[]) { ;
         Calculadora calcu1 = new Calculadora();
         System.out.println(calcu1.sumar(1,5));
+        System.out.println(calcu1.restar(5,1));
+        System.out.println(calcu1.dividir(10,2));
     }
 }
